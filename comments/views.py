6bced80f2b1ecd01494cf9870f937f167a483b56
Ajamal Khan <ajamalkhan65@gmail.com>
2021-03-14 17:54:01 +0530

    Add replies as subresource

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 from comments.models import Comment, Reply
 from comments.serializers import CommentSerializer, ReplySerializer
@@ -7,6 +10,14 @@ from comments.serializers import CommentSerializer, ReplySerializer
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
+
+    @action(detail=True)
+    def replies(self, request, *args, **kwargs):
+        comment = self.get_object()
+        replies = comment.replies.all()
+        return Response(ReplySerializer(
+            replies, many=True, context={"request": request}
+        ).data)
 
 
 class ReplyViewSet(viewsets.ModelViewSet):
